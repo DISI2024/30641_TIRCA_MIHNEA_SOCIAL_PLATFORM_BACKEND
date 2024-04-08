3044ce12c00bbbc247048b31576f0fa51a52bcec
Mihnea Tirca <Tirca.Fl.Mihnea@student.utcluj.ro>
2024-04-08 17:49:15 +0300

    init

---
+++ src/main/java/ro/disi/disi_backend/DisiBackendApplication.java
@@ -0,0 +1,13 @@
+package ro.disi.disi_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DisiBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DisiBackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ro/disi/disi_backend/DisiBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package ro.disi.disi_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DisiBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
