2a55f591c3f269fda29b15ed0f646742da90e2f7
carla <spaczai.zo.carla@student.utcluj.ro>
2024-05-20 01:04:08 +0300

    Delete Posts.

--- src/main/java/ro/disi/disi_backend/service/PostService.java
+++ src/main/java/ro/disi/disi_backend/service/PostService.java
@@ -2,6 +2,7 @@ package ro.disi.disi_backend.Service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 import ro.disi.disi_backend.Dto.PostDto;
 import ro.disi.disi_backend.model.entity.Post;
 import ro.disi.disi_backend.model.entity.UserProfile;
@@ -31,5 +32,20 @@ public class PostService {
         }
 
         return false;
+    }
+
+    @Transactional
+    public void deletePostByClient(Long postId, String username) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("Post not found!"));
+        if (!post.getUserProfile().getUser().getUsername().equals(username)) {
+            throw new SecurityException("You do not have permission to delete this post!");
+        }
+        postRepository.delete(post);
+    }
+
+    @Transactional
+    public void deletePostByAdmin(Long postId) {
+        Post post = postRepository.findById(postId).orElseThrow(() -> new IllegalArgumentException("Post not found!"));
+        postRepository.delete(post);
     }
 }
