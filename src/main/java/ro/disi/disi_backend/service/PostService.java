09d6bb583d5ea05d8d07104a8f655d3f884f6897
Mihnea Tirca <Tirca.Fl.Mihnea@student.utcluj.ro>
2024-05-20 15:20:13 +0300

    packname

--- src/main/java/ro/disi/disi_backend/service/PostService.java
+++ src/main/java/ro/disi/disi_backend/service/PostService.java
@@ -1,4 +1,4 @@
-package ro.disi.disi_backend.Service;
+package ro.disi.disi_backend.service;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
